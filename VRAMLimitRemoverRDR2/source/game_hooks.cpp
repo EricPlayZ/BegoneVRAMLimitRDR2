a9b3e73cbd82889bf8146e95e78d9b61e0c620c4
EricPlayZ <eric.jarnea12345361@gmail.com>
2024-04-07 00:14:37 +0300

    alternative way of overwriting VRAM amount

--- VRAMLimitRemoverRDR2/source/game_hooks.cpp
+++ VRAMLimitRemoverRDR2/source/game_hooks.cpp
@@ -3,11 +3,39 @@
 
 namespace Hooks {
 #pragma region VRAMCheck
+	static DWORD* pVRAMUsed = nullptr;
+	static DWORD* pVRAMUsed2 = nullptr;
+	static DWORD* pVRAMUsedApps = nullptr;
+
 	static bool detourVRAMCheck(LPVOID pInstance);
 	static Utils::Hook::MHook<LPVOID, bool(*)(LPVOID)> VRAMCheckHook{ "VRAMCheck", &Offsets::Get_VRAMCheck, &detourVRAMCheck };
 
 	static bool detourVRAMCheck(LPVOID pInstance) {
-		return false;
+		const DWORD VRAMUsedOffset = *Offsets::Get_VRAMUsedOffset();
+		pVRAMUsed = reinterpret_cast<DWORD*>((reinterpret_cast<DWORD64>(pInstance) + VRAMUsedOffset));
+		*pVRAMUsed = 0;
+
+		const DWORD VRAMUsed2Offset = *Offsets::Get_VRAMUsed2Offset();
+		pVRAMUsed2 = reinterpret_cast<DWORD*>((reinterpret_cast<DWORD64>(pInstance) + VRAMUsed2Offset));
+		*pVRAMUsed2 = 0;
+
+		const DWORD VRAMUsedAppsOffset = *Offsets::Get_VRAMUsedAppsOffset();
+		pVRAMUsedApps = reinterpret_cast<DWORD*>((reinterpret_cast<DWORD64>(pInstance) + VRAMUsedAppsOffset));
+		*pVRAMUsedApps = 0;
+
+		return VRAMCheckHook.pOriginal(pInstance);
+	}
+#pragma endregion
+
+#pragma region VRAMWriteFunc
+	static bool detourVRAMWriteFunc(DWORD* a1, DWORD64 a2, int a3, DWORD* a4, DWORD* a5);
+	static Utils::Hook::MHook<LPVOID, bool(*)(DWORD*, DWORD64, int, DWORD*, DWORD*)> VRAMWriteFuncHook{ "VRAMWriteFunc", &Offsets::Get_VRAMWriteFunc, &detourVRAMWriteFunc };
+
+	static bool detourVRAMWriteFunc(DWORD* a1, DWORD64 a2, int a3, DWORD* a4, DWORD* a5) {
+		if (a4 == pVRAMUsed || a4 == pVRAMUsed2 || a4 == pVRAMUsedApps)
+			return true;
+
+		return VRAMWriteFuncHook.pOriginal(a1, a2, a3, a4, a5);;
 	}
 #pragma endregion
 }
--- VRAMLimitRemoverRDR2/source/offsets.h
+++ VRAMLimitRemoverRDR2/source/offsets.h
@@ -11,6 +11,10 @@ static retType Get_## name () {\
 
 struct Offsets {
 	AddOffset(VRAMCheck, "RDR2.exe", "48 89 5C 24 ?? 55 56 57 41 54 41 55 41 56 41 57 48 8D 6C 24 ?? 48 81 EC ?? ?? ?? ?? 48 8B 1D", Utils::SigScan::PatternType::Address, LPVOID)
+	AddOffset(VRAMWriteFunc, "RDR2.exe", "E8 [?? ?? ?? ?? E9 ?? ?? ?? ?? 83 EE", Utils::SigScan::PatternType::RelativePointer, LPVOID)
+	AddOffset(VRAMUsedOffset, "RDR2.exe", "8B B7 [?? ?? ?? ?? 4C 8D BF", Utils::SigScan::PatternType::Address, DWORD*)
+	AddOffset(VRAMUsed2Offset, "RDR2.exe", "03 87 [?? ?? ?? ?? 39 46", Utils::SigScan::PatternType::Address, DWORD*)
+	AddOffset(VRAMUsedAppsOffset, "RDR2.exe", "8B 87 [?? ?? ?? ?? 48 8D B7 ?? ?? ?? ?? 03 87", Utils::SigScan::PatternType::Address, DWORD*)
 };
 
 #undef AddOffset
